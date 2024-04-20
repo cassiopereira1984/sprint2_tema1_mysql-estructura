d86b7d422a5472ee6d3622f5da753ae340ee98a5
cassiopereira1984 <159780983+cassiopereira1984@users.noreply.github.com>
2024-04-20 17:33:44 +0200

    Delete Pizzaria/sprint2_tema1_pizzaria_queris.sql

--- Pizzaria/sprint2_tema1_pizzaria_queris.sql
+++
@@ -1,4 +0,0 @@
-/*Lista cuántos productos de tipo “Bebidas” se han vendido en una determinada localidad.*/
-select sum(detalles.cantidad) as suma_bebidas_madrid from detalles inner join pedido on detalles.id_pedido = pedido.id_pedido inner join tienda on pedido.id_tienda = tienda.id_tienda inner join producto on detalles.id_producto = producto.id_producto where producto.tipo_producto = "bebida" and tienda.localidad = "madrid";
-/*Lista cuántos pedidos ha efectuado un determinado empleado/a.*/
-select empleado.nombre, count(empleado.nombre) from empleado left join tienda on empleado.id_empleado = tienda.id_tienda left join pedido on tienda.id_tienda = pedido.id_tienda where empleado.nombre = "pablo"
